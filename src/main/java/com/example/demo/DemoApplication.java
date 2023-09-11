ae4d71204a8c2ee3adeec110b32735a6710ca73e
LSM <dltmdals6718@naver.com>
2023-09-11 19:42:44 +0900

    mysql 연동 및 데이터 반영 완료

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
-import com.example.demo.domain.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.Entity;
 
 @SpringBootApplication
+@EntityScan
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
