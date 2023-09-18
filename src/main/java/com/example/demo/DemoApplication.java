3bf9b75c88a2720aacffe17b7d34de2a0256d520
ysbin0505 <chfhddl515@gmail.com>
2023-09-18 19:18:14 +0900

    ysb branch 커밋

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import javax.persistence.Entity;
 
 @SpringBootApplication
 public class DemoApplication {
